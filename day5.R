78fe3512779942f8b0fe10bcd36a964d3164331b
rachel.bunder <rachelbunder@gmail.com>
2019-12-07 15:54:04 +1100

    Horribly messy, but working solution

--- day5.R
+++ day5.R
@@ -5,7 +5,7 @@ library(stringr)
 run_ops_codes <- function()
 {
   # The expected number of parameters for the code
-  number_params <- c('01'=3, '02'=3, '03'=1, '04'=1)
+  number_params <- c('01'=3, '02'=3, '03'=1, '04'=1, '05'=2, '06'=2, '07'=3, '08'=3)
   
   op_codes <- scan('day5.txt', sep=',', quiet=TRUE)
   i <- 1
@@ -42,6 +42,7 @@ run_ops_codes <- function()
     print(paste('code', code))
     parameters <- parameters[1:number_params[code]]
     parameters[is.na(parameters)] <- '0'
+    print(parameters)
  
     # Reassign the value of each parameter to be what is actually needed
     # I'm pretty sure this could be done in lapply, but I'm not sure how to get
@@ -51,8 +52,9 @@ run_ops_codes <- function()
     {
       # if in position mode, look at the value in the position
       # If writing, we ust want the location value, not the value in it
-      if(p==3)
+      if(p==3 | (p==2 & param=='1' & (code=='05' | code=='06')))
       {
+        print(paste(p, op_codes[i+p]))
         parameters[p] <- op_codes[i+p]
       }
     # Reading, always going to save to the right place
@@ -61,19 +63,14 @@ run_ops_codes <- function()
       parameters[p] <- op_codes[i+p]
     }
       # if in relative mode, it is just itself. 
-      else if(param == '1' | code =='03')
+      else if(param == '1')
       {
-        print(paste('parameter', p, parameters[p]))
-        parameters[p] <- parameters[p]
+       # print(paste('parameter', p, parameters[p]))
+        parameters[p] <- op_codes[i + p]
       }
       else if(param == '0')
       {
           parameters[p] <- op_codes[op_codes[i+p]+1]
-      }
-      # if in relative mode, it is just itself,but if we are writing the next one
-      else if(param=='1' & code=='04')
-      {
-        
       }
       else
       {
@@ -82,42 +79,97 @@ run_ops_codes <- function()
       p <- p + 1
     }
     parameters <- strtoi(parameters)
-    print(paste(i, code, instruction))
+   # print(paste(i, code, instruction))
     # Adds
     if(code=='01')
     {
-      op_codes[parameters[3]] <- parameters[1] + parameters[2]
+      op_codes[parameters[3]+1] <- parameters[1] + parameters[2]
       step_size <- 4
       print(paste('adds', parameters[1], parameters[2], 'saves to', parameters[3]))
     }
     # Multiplies
     else if (code=='02')
     {
-      op_codes[parameters[3]] <-  parameters[1] * parameters[2]
+      op_codes[parameters[3]+1] <-  parameters[1] * parameters[2]
       step_size <- 4
     }
     # Value to position
     else if (code == '03')
     {
-      op_codes[parameters[1] + 1] <- 1# readline(prompt='Input needed: ') %>% as.integer()
-      print(paste('writes',1, 'to', parameters[1]))
+      op_codes[parameters[1] + 1] <- readline(prompt='Input needed: ') %>% as.integer()
+      #print(paste('writes',1, 'to', parameters[1]+1))
       step_size <- 2
     }
     # Print
     else if (code=='04')
     {
-      print(op_codes[parameters[1]])
+      #print(op_codes[parameters[1]])
+      print(parameters[1])
       step_size <- 2
       
-      print(paste('in func', i))
-      return(op_codes)
+    #  print(paste('in func', i))
     }
-    else if (op_codes[i]==99)
+    else if (code=='05')
+    {
+      if(parameters[1]!= 0)
+      {
+        i <- parameters[2] + 1  # pretty sure about needing to offfset it by -1
+        print(paste('Next iteration', i, 'as ', parameters[1], 'is non-zero'))
+        step_size <- 0
+      }
+      else
+      {
+        print(paste('No jump', parameters[1]))
+        step_size <- 3
+      }
+    }
+    else if (code=='06')
+    {
+      if(parameters[1]== 0)
+      {
+        i <- parameters[2] + 1 # pretty sure about needing to offfset it by -1
+        step_size <- 0
+        print(paste('jump to',i, 'as', parameters[1], 'is true'))
+      }
+      else
+      {
+        print(paste('No jump', parameters[1]))
+        step_size <- 3
+      }
+    }
+    else if (code=='07')
+    {
+      if(parameters[1] < parameters[2])
+      {
+        op_codes[parameters[3]+1] <- 1
+      }
+      else
+      {
+        op_codes[parameters[3]+1] <- 0
+      }
+      step_size <- 4
+    }
+    else if (code=='08')
+    {
+      if(parameters[1] == parameters[2])
+      {
+        op_codes[parameters[3]+1] <- 1
+        #print(paste('Writing 1 to', parameters[3]+1, 'as', parameters[1], parameters[2], 'are equal'))
+      }
+      else
+      {
+        op_codes[parameters[3]+1] <- 0
+       # print(paste('Writing 0 to', parameters[3]+1, 'as', parameters[1], parameters[2], 'are unequal'))
+      }
+      step_size <- 4
+    }
+    else if (op_codes[i]=='99' )
     {
       break
     }
     else
     {
+      #print(code)
       print("Didn't meet conditions, investigate")
     }
     i <- i + step_size
@@ -126,5 +178,6 @@ run_ops_codes <- function()
 }
 result <- run_ops_codes()
 
-
 result
+
+#result
